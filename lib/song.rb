a6b34eb244de5b4b90af76bea8b07f449df5afe6
Tony Distinti <adistinti@gmail.com>
2017-09-29 13:07:33 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -4,28 +4,21 @@ class Song
 
   def initialize(name)
     @name = name
-    @artist = artist_name(name) #This is all wrong but this is where I am up to -- may need to instantiate artist when you instantiate song
   end
 
   def self.new_by_filename(filename)
     song_title = filename.split(" - ")[1]
     self.new(song_title)
-    name = filename.split(" - ")[0]
-    artist_name=(name)
+    artist_name = filename.split(" - ")[0]
+    artist_name=(artist_name)
     artist.add_song(song_title)
-    # song_list = [ ]
-    # files.each do |x|
-    #   song_list <<  x.split(" - ")[1]
-    # end
-    # song_list.each { |y| Song.new(y) }
-
   end
 
-  def artist_name=(name)
+  def artist_name=(artist_name)
     if (self.artist.nil?) #TN: note this!
-      self.artist = Artist.new(name)
+      self.artist = Artist.new(artist_name)
     else
-      self.artist.name = name
+      self.artist.name = @name
     end
   end
 
