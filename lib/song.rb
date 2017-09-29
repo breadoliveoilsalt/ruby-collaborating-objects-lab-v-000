2aaf9b8df9cef1c28da624fa2160a2a41a4e0238
Tony Distinti <adistinti@gmail.com>
2017-09-29 13:24:32 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -8,14 +8,15 @@ class Song
 
   def self.new_by_filename(filename)
     song_title = filename.split(" - ")[1]
-    self.new(song_title)
+    s = self.new(song_title)
     artist_name = filename.split(" - ")[0]
     artist_name=(artist_name)
+    s
   #  artist.add_song(song_title)
   end
 
   def artist_name=(artist_name)
-    if (self.artist.nil?) #TN: note this!
+    if (self.artist.nil?)
       self.artist = Artist.new(artist_name)
     else
       self.artist.name = @name
